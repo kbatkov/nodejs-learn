6bc8035a4201821120bbc025f06800c9b29702f3
kbatkov <kbatkovinfo@gmail.com>
2022-10-05 20:04:12 +0300

    #3 Pug

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
 
@@ -30,7 +33,8 @@ app.use('/admin', adminData.router)
 app.use(shopRoutes)
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
+    res.status(404).render('404', {pageTitle: 'Page not found! :('})
 })
 
 // const server = http.createServer(app)
--- routes/shop.js
+++ routes/shop.js
@@ -7,8 +7,10 @@ const router = express.Router()
 const adminData = require('./admin')
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products)
-    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    // console.log(adminData.products)
+    // res.sendFile(path.join(rootDir, 'views', 'shop.html'))
+    const products = adminData.products
+    res.render('shop', {prods: products, pageTitle: 'Shop'})
 })
 
 module.exports = router
